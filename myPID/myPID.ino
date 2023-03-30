c8d592bce8b3c4ff522afba91d9d04b348070511
AntonioCdR-2kX <antcameo2020@gmail.com>
2023-03-30 12:56:14 +0200

    Corrección de errores

--- myPID/myPID.ino
+++ myPID/myPID.ino
@@ -3,22 +3,20 @@
 #include "Wire.h"
 
 
-
 /* SETTINGS */
-#define ANGLE_FILTER_G    = 0.999 // gyro portion of complementary filter [0...1]
-#define ANGLE_LIMIT       = 18    // stop program / start rise up sequence [deg]
-#define RISEUP_END_ANGLE  = 5     // stop rise up sequence and start balancing [deg]
-#define ANGLE_FIXRATE     = 1.0   // variate target angle [deg/s]
-#define ANGLE_FIXRATE_2   = 0.1   // reduce continuous rotation
-#define KP                = 0.2   // PID proportional factor
-#define KI                = 0.5   // PID integral factor
-#define KD                = 0.005 // PID derivative factor
-#define MOTOR_R           = 13    // motor resistance [Ohm]
-#define MOTOR_Ke          = 0.26  // motor back EMF constant [Vs/rad]
-#define SUPPLY_VOLTAGE    = 8.1   // battery box voltage [V]
-#define WHEEL_AV_INTERVAL = 100   // wheel angular velocity calculation interval [ms]
-#define SLEEP_TIME        = 1     // main loop sleep [ms]
-
+#define ANGLE_FILTER_G     0.999 // gyro portion of complementary filter [0...1]
+#define ANGLE_LIMIT        18    // stop program / start rise up sequence [deg]
+#define RISEUP_END_ANGLE   5     // stop rise up sequence and start balancing [deg]
+#define ANGLE_FIXRATE      1.0   // variate target angle [deg/s]
+#define ANGLE_FIXRATE_2    0.1   // reduce continuous rotation
+#define KP                 0.2   // PID proportional factor
+#define KI                 0.5   // PID integral factor
+#define KD                 0.005 // PID derivative factor
+#define MOTOR_R            13    // motor resistance [Ohm]
+#define MOTOR_Ke           0.26  // motor back EMF constant [Vs/rad]
+#define SUPPLY_VOLTAGE     8.1   // battery box voltage [V]
+#define WHEEL_AV_INTERVAL  100   // wheel angular velocity calculation interval [ms]
+#define V_MAX              255   // 
 
 
 /* VARIABLES */ 
@@ -28,7 +26,7 @@ const int mpuAddress = 0x68;  // Puede ser 0x68 o 0x69
 MPU6050 mpu(mpuAddress);
 
 int ax, ay, az;
-int gx, gy, gz;
+//int gx, gy, gz;
 
 // motor
 const int pinPWMA = 5;
@@ -37,10 +35,10 @@ const int pinAIN1 = 7;
 const int pinMotorA[3] = { pinPWMA, pinAIN2, pinAIN1 };
 const int pinSTBY = 8;
 
-enum moveDirection {
-   forward,
-   backward
-};
+//enum moveDirection {
+//   forward,
+//   backward
+//}
 int speed = 0;      //velocidad de giro
 
 // encoder
@@ -71,10 +69,7 @@ float PIDoutput = 0;
 float motorCtrl = 0;
 
 
-enum risingUp {
-   true,
-   false
-};
+bool risingUp;
 
 /* INICIALIZO */
 void setup()
@@ -115,45 +110,62 @@ void loop()
    // Leer las aceleraciones 
    mpu.getAcceleration(&ax, &ay, &az);
    //Calcular los angulos de inclinacion
-   float accel_ang_x = atan(ax / sqrt(pow(ay, 2) + pow(az, 2)))*(180.0 / 3.14);
+   float accAngle = atan(ax / sqrt(pow(ay, 2) + pow(az, 2)))*(180.0 / 3.14);
 
+   // Read gyroscope
+   //mpu.readGyroscopeMaster(&gx, &gy, &gz);  // [deg/s]
+   //float gyroAngleDelta = gz * timeDelta;
+   //if isnan(gyroAngle) gyroAngle = accAngle;
+   //gyroAngle += gyroAngleDelta;  // [deg]
+   
+   //calculate arm angle (complementary filter)
+   //if isnan(measuredAngle) measuredAngle = accAngle;
+   //measuredAngle = (ANGLE_FILTER_G * (measuredAngle + gyroAngleDelta) + (1-ANGLE_FILTER_G) * accAngle);  // [deg]
+   
+   // Cálculo sin filtro
+   measuredAngle = accAngle;
 
    // Safety check
-    if (abs(accel_ang_x) >= ANGLE_LIMIT) {
+    if (abs(measuredAngle) >= ANGLE_LIMIT) 
+    {
         if (secondsSinceStart < 0.01) {
             Serial.println("START RISE UP SEQUENCE");
             risingUp = true;
         }
         else if(!risingUp) {
-            Serial.println("PROGRAM STOPPED, angle is too large: %.2f" % (measuredAngle));
+            Serial.print("PROGRAM STOPPED, angle is too large: ");
+            Serial.println(measuredAngle);
+            fullStop();
         }        
     }
 
     
     // Calculate wheel angular velocity
-    if (((millis() - prevTachoTime) / 1e6 )>= WHEEL_AV_INTERVAL){
-        tachoTimeDelta = (millis() - prevTachoTime) / 1e9;  // [sec]
+    if (((millis() - prevTachoTime) / 1e6 )>= WHEEL_AV_INTERVAL)
+    {
+        float tachoTimeDelta = (millis() - prevTachoTime) / 1e9;  // [sec]
         prevTachoTime = millis();
         
-        pulses = tachoCount - prevTachoCount;
-        prevTachoCount = tachoCount;
-        cycles = pulses / 360;   // 360 pulses per rotation
+        float pulses = contador - prevTachoCount;
+        prevTachoCount = contador;
+        float cycles = pulses / 360;   // 360 pulses per rotation
         wheelAV = cycles / tachoTimeDelta * 2 * 3.14;  // [rad/s]
     }
         
 
     // Rise up sequence, usamos el doble impulso meteoro inverso
-    if (risingUp) {
+    if (risingUp) 
+    {
         if (secondsSinceStart < 1.0) {
             // speed up reaction wheel to full speed
-            if (measuredAngle < 0) move(forward, int speed);
-            else if (measuredAngle > 0) move(backward, int speed);
+            if (measuredAngle < 0) move(-1);
+            else if (measuredAngle > 0) move(1);
         }
         else if (secondsSinceStart < 1.5){
             // change direction using full power
-            if (measuredAngle > 0) move(forward, int speed);
+            if (measuredAngle > 0) move(V_MAX);
             else if (measuredAngle < 0){
-                move(backward, int speed);
+                move(-V_MAX);
                 // wait until close to top position, then start balancing
                 if (abs(measuredAngle) < RISEUP_END_ANGLE) {
                     Serial.println("END RISE UP SEQUENCE");
@@ -162,10 +174,11 @@ void loop()
             } 
         }
         else {
-            print("RISE UP TIMEOUT");
+            Serial.println("RISE UP TIMEOUT");
         }     
     }
-    else if (!risingUp) {
+    else if (!risingUp) 
+    {
         // variate target angle
         if (measuredAngle < targetAngle) targetAngle += ANGLE_FIXRATE * timeDelta;
         else targetAngle -= ANGLE_FIXRATE * timeDelta;
@@ -181,32 +194,37 @@ void loop()
         PIDoutput = KP * error + KI * integral + KD * derivative;
         
         // compensate for motor back EMF voltage
-        current = -PIDoutput;
-        voltage = MOTOR_R * current + MOTOR_Ke * wheelAV;
+        float current = -PIDoutput;
+        float voltage = MOTOR_R * current + MOTOR_Ke * wheelAV;
         
         //convert voltage to pwm duty cycle
         motorCtrl = voltage / SUPPLY_VOLTAGE;
         
         // drive motor
-        if (motorCtrl > 0) move(forward, abs(motorCtrl));
-        else if (motorCtrl < 0) move(backward, int speed);
+        motorCtrl = min(max(motorCtrl, -1), 1);
+        move(motorCtrl);
     }
 
     // debug print
-    if (((millis() - prevPrintTime) / 1e9) >= 1.0){
-        secondsSinceLastPrint = (millis() - prevPrintTime) / 1e9;
+    if (((millis() - prevPrintTime) / 1e9) >= 1.0)
+    {
+        float secondsSinceLastPrint = (millis() - prevPrintTime) / 1e9;
         prevPrintTime = millis();
-        loopInterval = secondsSinceLastPrint / loopCount * 1000;
+        float loopInterval = secondsSinceLastPrint / loopCount * 1000;
         loopCount = 0;
-        Serial.println("measuredAngle: %.2f, motorCtrl: %.2f, loopInterval: %.2f ms"
-              % (measuredAngle, motorCtrl, loopInterval));
+        Serial.print("measuredAngle: ");
+        Serial.print(measuredAngle);
+        Serial.print(", motorCtrl: ");
+        Serial.print(motorCtrl);
+        Serial.print(", loopInterval: ");
+        Serial.println(loopInterval);
     }
 
     loopCount += 1;
-
+   
    // Mostrar resultados IMU
    Serial.print("Inclinacion en X: ");
-   Serial.println(accel_ang_x);
+   Serial.println(accAngle);
 
    // Mostrar resultado encoder
    Serial.print("Encoder: ");
@@ -219,8 +237,8 @@ void loop()
 
 
 
-/* FUNCIONES CONTADOR DEL ENCODER*/
- void Lectura_encoderA() {
+/* FUNCIONES CONTADOR DEL ENCODER */
+void Lectura_encoderA() {
      int b = digitalRead(CanalB);
      if (b > 0) {
          contador++;
@@ -230,7 +248,7 @@ void loop()
      }
 }
 
- void Lectura_encoderB() {
+void Lectura_encoderB() {
      int b = digitalRead(CanalA);
      if (b > 0) {
          contador++;
@@ -243,17 +261,19 @@ void loop()
 
 
 /* FUNCIONES CONTROL DEL MOTOR*/
-void move(int direction, int speed)
+void move(int per_speed) 
 {
-   if (direction == forward) moveMotorForward(pinMotorA, speed);
-   else moveMotorBackward(pinMotorA, speed);
+   // Speed puede tener valores positivos y negativos
+   if (per_speed >= 0) moveMotorForward(pinMotorA, V_MAX * per_speed);
+   else if (per_speed < 0) moveMotorBackward(pinMotorA, V_MAX * per_speed);
 }
 
-void turn(int direction, int speed)
-{
-   if (direction == forward) moveMotorForward(pinMotorA, speed);
-   else moveMotorBackward(pinMotorA, speed);
-}
+//void turn(int per_speed)
+//{
+//   // Speed puede tener valores positivos y negativos
+//   if (per_speed >= 0) moveMotorForward(pinMotorA, V_MAX * per_speed);
+//   else if (per_speed < 0) moveMotorBackward(pinMotorA, V_MAX * per_speed);
+//}
 
 void fullStop()
 {
@@ -292,6 +312,3 @@ void disableMotors()
 {
    digitalWrite(pinSTBY, LOW);
 }
-
-
-
